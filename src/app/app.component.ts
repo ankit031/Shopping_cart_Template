aa80bd3e4b6443c2ca1a7a8f2e83ad0495818088
ankit031 <ankitsinha031@gmail.com>
2019-07-08 12:51:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(@Inject(HttpClient) public ht){
+
+  }
   title = 'task3';
+  dataInfo:object[]
+  info:object[]
+  qnty:number=0;
+  ngOnInit () {
+    this.ht.get('./assets/data.json').subscribe(
+      data => {
+        this.dataInfo = data as object [];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.arrBirds[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+  }
+
+  fun1(){
+    this.ht.get('./assets/data.json').subscribe((dt)=>{
+      this.info = (dt)
+    })
+  }
 }
